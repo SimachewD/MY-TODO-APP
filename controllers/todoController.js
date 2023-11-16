fe2541e59bc4f5698fea5bc07535328b1e0ea1a1
Simachew <simachewdenekewse@gmail.com>
2023-11-16 15:51:45 +0300

    Second commit of my TODO app with MongoDB

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,24 +1,53 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{ item: 'Get Milk' }, { item: 'Walk Dog' }, { item: 'Kick some coding ass' }];
+//connect to the database
+mongoose.connect('mongodb+srv://Sime:3165@todo.gxnxhsq.mongodb.net/?retryWrites=true&w=majority');
+
+//create schema - it's like a blueprint
+var todoSchema = new mongoose.Schema({
+    item:String
+});
+
+// create a model
+   var Todo = mongoose.model('Todo', todoSchema);
+// var item1 = Todo({item:'buy flowers'}).save()
+
+
+// var data = [{ item: 'Get Milk' }, { item: 'Walk Dog' }, { item: 'Kick some coding ass' }];
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function (app) {
-    app.get('/todo', function (req, res) {
-        res.render('todo', { todos: data });
+    app.get('/todo', async function (req, res) {
+        try {
+            const data = await Todo.find();
+            res.render('todo', { todos: data });
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            res.status(500).send('Internal Server Error');
+        }
     });
 
-    app.post('/todo', urlencodedParser, function (req, res) {
-        data.push(req.body);
-        res.json(data);
+    app.post('/todo', urlencodedParser, async function (req, res) {
+        try {
+            const newData = new Todo(req.body);
+            const data = await newData.save();
+            res.json(data);
+        } catch (error) {
+            console.error('Error saving data:', error);
+            res.status(500).send('Internal Server Error');
+        }
     });
 
-    app.delete('/todo/:item', function (req, res) {
-        // Delete the item from the data array
-        data = data.filter(function (todo) {
-            return todo.item.replace(/ /g, "-") !== req.params.item;
-        });
-        res.json(data);
+    app.delete('/todo/:item', async function (req, res) {
+        try {
+            const data = await Todo.findOneAndDelete({ item: req.params.item.replace(/\-/g, " ") });
+            res.json(data);
+        } catch (error) {
+            console.error('Error deleting data:', error);
+            res.status(500).send('Internal Server Error');
+        }
     });
 };
+
